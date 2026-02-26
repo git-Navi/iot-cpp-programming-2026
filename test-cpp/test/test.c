f028f18484d3ae90f9522656d7ba14f73d658618
git-Navi <122284636+git-Navi@users.noreply.github.com>
2026-02-26 17:21:43 +0900

    commit c++ 1일차

---
+++ test-cpp/test/test.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int n = 3.14;
+
+int main() {
+	char arr[] = "hong";
+	char* arr1 = "hong";
+
+	//printf("arr: %c", arr);
+	//printf("arr[0] : %c\n", arr[0]);
+	//printf("arr[1] : %c\n", arr[1]);
+	//printf("arr[2] : %c\n", arr[2]);
+	//printf("arr[3] : %c\n", arr[3]);
+
+	printf("arr: %p\n", arr);
+	printf("arr1: %p\n", &arr1);
+	printf("hong: %p\n", *arr1);
+	printf("n: %p\n", &n);
+
+	return 0;
+}
---
+++ test-cpp/test2/test2.c
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+
+int a;
+int global_C = 100;
+int func() {
+	return 1;
+}
+
+int main() {
+	int b;
+	static int c;
+	const int d = 10;
+	char arr[20] = "hi";
+	char* parr = "hong";
+
+	printf("func     : %p\n", &func);
+	printf("global a : %p\n", &a);
+	printf("local b  : %p\n", &b);
+	printf("global_C : %p\n", &global_C);
+	printf("static c : %p\n", &c);
+	printf("const d  : %p\n", &d);
+	printf("array    : %p\n", arr);
+	printf("parr     : %p\n", parr);
+
+	return 0;
+}
+
+/*-- 메모리 구조 --*/
+
+/*
+
+	data segment : stack, heap, data
+	code segment : s
+
+*/
