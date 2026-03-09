228b3628924dca7925cda6890a4ee79aed4e0d03
git-Navi <122284636+git-Navi@users.noreply.github.com>
2026-03-09 17:29:46 +0900

    commit

--- space-cpp/Chap1/Basic/06_function_args1.cpp
+++ space-cpp/Chap1/Basic/06_function_args1.cpp
@@ -1,22 +1,22 @@
-#include <iostream>
-using namespace std;
-
-void func(int y);
-
-int main() {
-	
-	int x = 10;
-
-	func(x);
-
-	cout << "main 함수 내부의 x는 " << x << "입니다." << endl;
-
-	return 0;
-}
-
-void func(int y) {
-	y++;
-	cout << "func 함수 내부의 y는 " << y << "입니다. "<< endl;
-
-	return;
-}
+//#include <iostream>
+//using namespace std;
+//
+//void func(int y);
+//
+//int main() {
+//	
+//	int x = 10;
+//
+//	func(x);
+//
+//	cout << "main 함수 내부의 x는 " << x << "입니다." << endl;
+//
+//	return 0;
+//}
+//
+//void func(int y) {
+//	y++;
+//	cout << "func 함수 내부의 y는 " << y << "입니다. "<< endl;
+//
+//	return;
+//}
